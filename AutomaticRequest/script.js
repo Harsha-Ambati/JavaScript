75cf1efec079b82b224baa952ba17dc567916c3a
harsha vardhan <47480010+Harsha-Ambati@users.noreply.github.com>
2020-05-14 14:38:25 +0530

    Update script.js

--- AutomaticRequest/script.js
+++ AutomaticRequest/script.js
@@ -1,44 +1,44 @@
-var skipCoder = false;
-var skipCoderCount = 0;
+var skipLink = false;
+var skipLinkCount = 0;
 var lastOne = "";
-function connectCoders(){
+function connectPeople(){
 
     //Scroll to bottom of page
     window.scrollTo(0,document.body.scrollHeight);
 
-    // Find all coders listed on page as search results.
-    // Store them in variable "coders".
-    var coders = document.getElementsByClassName('search-result__actions--primary');
+    // Find all people listed on page as search results.
+    // Store them in variable "cybersec".
+    var cybersec = document.getElementsByClassName('search-result__actions--primary');
 
     var found = false;
 
     //Loop on every search result of this page !
     var countSkipFew = 0;
-    for (var i =0; i<coders.length; i++){
-      // Check coder for which it shows option of "Connect" as button !
-      if(coders[i].innerText===("Connect")){
-        if( skipCoder && countSkipFew < skipCoderCount){
+    for (var i =0; i<cybersec.length; i++){
+      // Check people for which it shows option of "Connect" as button !
+      if(cybersec[i].innerText===("Connect")){
+        if( skipLink && countSkipFew < skipLinkCount){
             countSkipFew++;
             continue;
         }
         //Click "Connect"
-        coders[i].click();
+        cybersec[i].click();
         //coder was found
         found = true;
 
-        //Print name of coder, we just connected.
+        //Print name of people, we just connected.
         try{
 
             // In case script get stuck !
-            if(lastOne===coders[i].getAttribute("aria-label").split(". ")[0]){
+            if(lastOne===cybersec[i].getAttribute("aria-label").split(". ")[0]){
                 window.scrollTo(0,document.body.scrollHeight);
                 document.querySelector('[aria-label="Next"]').click();
-                skipCoder = false;
-                skipCoderCount = 0;
+                skipLink = false;
+                skipLinkCount = 0;
                 return;
             }
 
-            lastOne = coders[i].getAttribute("aria-label").split(". ")[0];
+            lastOne = cybersec[i].getAttribute("aria-label").split(". ")[0];
             console.log(lastOne);
 
         }catch(err){}
@@ -56,8 +56,8 @@ function connectCoders(){
     if(found){
         try{
             if(!!document.querySelector('label[for="email"]')){
-                skipCoder = true;
-                skipCoderCount++;
+                skipLink = true;
+                skipLinkCount++;
             }
 
             // Check if any pop up comes then send a request by clicking "Send Now"
@@ -74,21 +74,21 @@ function connectCoders(){
         }catch(err){
         }
     }
-   // If no coder was found on this page lets try next page
+   // If no people was found on this page lets try next page
     else{
 		if(!!document.querySelector('[data-test="no-results-cta"]')){
             //If page is showing error page, refresh the results !
             document.querySelector('[data-test="no-results-cta"]').click();
         }else{
-            // Click and load next page as no coders left on this page !
+            // Click and load next page as no cybersec left on this page !
             // scroll bottom
             window.scrollTo(0,document.body.scrollHeight);
             document.querySelector('[aria-label="Next"]').click();
-            skipCoder = false;
-            skipCoderCount = 0;
+            skipLink = false;
+            skipLinkCount = 0;
         }
     }
 }
 
-setInterval(connectCoders,5000);
-//Connect one coder per 5 seconds.
+setInterval(connectPeople,5000);
+//Connect one people per 5 seconds.
